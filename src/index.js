7ff258e9691b626ec127092a61f277bbeb9abca5
Errol Sidelsky <errol@squie.com>
2018-02-06 13:49:43 +0000

    Video list update

--- src/index.js
+++ src/index.js
@@ -34,7 +34,12 @@ class App extends Component {
             <div>
                 <SearchBar />
                 <VideoDetail video={this.state.selectedVideo} />
-                <VideoList videos={this.state.videos} />
+                <VideoList
+                    onVideoSelect={selectedVideo =>
+                        this.setState({ selectedVideo })
+                    }
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
